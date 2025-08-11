104733b5b27f7c251fec9c7e3ab3cf958edbbbb0
yesung <jys_troy@naver.com>
2025-08-11 18:20:04 +0900

    .

---
+++ src/app/_global/styles/color.ts
@@ -0,0 +1,16 @@
+import { warn } from 'console'
+
+const color = {
+  primary: '#0d6efd',
+  secondary: '#6c757d',
+  success: '#198754',
+  info: '#0dcaf0',
+  warning: '#ffc107',
+  danger: '#dc3545',
+  light: '#f8f9fa',
+  dark: '#212529',
+  white: '#fff',
+  black: '#000',
+}
+
+export default color
